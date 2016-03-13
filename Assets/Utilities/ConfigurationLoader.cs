58bba6a65012ff3e6773633d355ee395939ca817
iamtanmay <iamtanmay@gmail.com>
2016-03-13 21:22:07 +0100

    - Init

---
+++ Assets/Utilities/ConfigurationLoader.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+namespace WorldUtilities
+{
+    public static class ConfigurationLoader
+    {
+        public string ConfigPath = Application.dataPath + "\\Internal\\Config\\";
+
+        public void SaveConfiguration(Dictionary<string, string> iconfig, string iID)
+        {
+            SaveConfigurationInternal(iconfig, ConfigPath + iID);
+        }
+
+        public Dictionary<string, string> LoadConfiguration(string iID)
+        {
+            return LoadConfigurationInternal(ConfigPath + iID);
+        }
+
+        void SaveConfigurationInternal(Dictionary<string, string> iconfig, string path)
+        {
+            string ttext = "";
+            foreach (KeyValuePair<string, string> tentry in iconfig)
+            {
+                string tstr = tentry.Key.Replace(" ", "_") + " = " + tentry.Value.Replace(" ", "_");
+                ttext = ttext + tstr + "\r\n";
+            }
+
+            byte[] tbytes = GetBytes(ttext);
+            File.WriteAllBytes(path, tbytes);
+        }
+
+        Dictionary<string, string> LoadConfigurationInternal(string path)
+        {
+            Dictionary<string, string> tconfig = new Dictionary<string,string>();
+            byte[] tbytes = File.ReadAllBytes(path);
+            string ttext = GetString(tbytes);
+            string[] ttextarr = ttext.Split(new string[] {"\n", "\r\n"}, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < ttextarr.Length; i++)
+            {
+                string[] tarr = ttextarr[i].Split("=".ToCharArray());
+                string tname = tarr[0].Replace(" ", "").Replace("_", " ");
+                string tval = tarr[1].Replace(" ", "").Replace("_", " ");
+                tconfig.Add(tname, tval);
+            }
+            return tconfig;
+        }
+
+        static byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        static string GetString(byte[] bytes)
+        {
+            char[] chars = new char[bytes.Length / sizeof(char)];
+            System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+            return new string(chars);
+        }
+    }
+}
